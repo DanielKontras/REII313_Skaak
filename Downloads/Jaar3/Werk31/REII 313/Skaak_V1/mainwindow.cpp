91c52ad8b7cd0d66064970bac9fcef8cd0e60786
DanielKontras <dnlkritzinger@gmail.com>
2023-05-30 13:01:15 +0200

    Demo

--- Downloads/Jaar3/Werk31/REII 313/Skaak_V1/mainwindow.cpp
+++ Downloads/Jaar3/Werk31/REII 313/Skaak_V1/mainwindow.cpp
@@ -1,15 +1,64 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    bord.resize(12);
+    for (int i = 0; i < 12; ++i)
+        bord[i].resize(12);
+
+    initBoard();
+
+    thelabel = new QLabel(this);
+    thelabel->setPixmap(QPixmap(":/img/pion.jpg").scaled(100,100));
+    thelabel->setFixedSize(thelabel->pixmap()->height(), thelabel->pixmap()->width());
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+
 }
 
+void MainWindow::initBoard()
+{
+    // frame top bottom
+    for (int i = 0; i < 12; ++i)
+    {
+        bord[0][i] = outside;
+        bord[1][i] = outside;
+        bord[10][i] = outside;
+        bord[11][i] = outside;
+    }
+    // frame sides
+    for (int j = 2; j < 10; ++j)
+    {
+        bord[j][0] = outside;
+        bord[j][1] = outside;
+        bord[j][10] = outside;
+        bord[j][11] = outside;
+    }
+    // empty space in middle
+    for (int i = 2; i < 10; ++i)
+    {
+        bord[3][i] = swartpion;
+        bord[4][i] = empty;
+        bord[5][i] = empty;
+        bord[6][i] = empty;
+        bord[7][i] = empty;
+        bord[8][i] = witpion;
+    }
+    // last two rows pieces
+    bord[2][2] = swartkasteel;
+    // etc
+
+    for (int x = 0; x < 12; ++x)
+    {
+        for (int y = 0; y < 12; ++y)
+        {
+            qDebug() << bord[x][y] << " ";
+        }
+        qDebug() << "\n";
+    }
+
+}
--- Downloads/Jaar3/Werk31/REII 313/Skaak_V1/mainwindow.h
+++ Downloads/Jaar3/Werk31/REII 313/Skaak_V1/mainwindow.h
@@ -2,20 +2,45 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QVector>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+enum ChessPiece
+{
+    outside = 99,
+    empty = 0,
+    witpion = 1,
+    swartpion = -1,
+    witperd = 2,
+    swartperd = -2,
+    witloper = 3,
+    swartloper = -3,
+    witkasteel = 4,
+    swartkasteel = -4,
+    witkoningin = 5,
+    swartkoningin = -5,
+    witkoning = 6,
+    swartkoning = -6
+};
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);//dfhadhdafdfadfha
+    MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+    QLabel *thelabel;
+    QVector<QVector<ChessPiece> > bord;
+
+    // display bord
+    // for example:
+    // QLabel* displayboard[8][8];
+
+    void initBoard();
+
 };
+
 #endif // MAINWINDOW_H
