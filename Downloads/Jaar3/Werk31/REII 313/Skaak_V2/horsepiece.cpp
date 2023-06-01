913e2c6762cdf17ada7ef8d5714ca9077fac9248
CP65 <neilltuck02@gmail.com>
2023-06-01 15:18:05 +0200

    Toets 1 2 3

--- Downloads/Jaar3/Werk31/REII 313/Skaak_V2/horsepiece.cpp
+++ Downloads/Jaar3/Werk31/REII 313/Skaak_V2/horsepiece.cpp
@@ -4,13 +4,139 @@ HorsePiece::HorsePiece(QWidget *parent, const Board &b, PieceColour clr)
     : ChessPiece(parent, b, clr)
 {
    // setText(pieceColour == Black ? "Swart\nPerd" : "Wit\nPerd");
-    setPixmap(pieceColour == Black ? QPixmap("C:/Users/dnlkr/Downloads/Jaar3/Werk31/REII 313/PRAC2/swartPerd.png").scaled(40, 40) :
-                                          QPixmap("C:/Users/dnlkr/Downloads/Jaar3/Werk31/REII 313/PRAC2/witPerd.png").scaled(40, 40));
+    setPixmap(pieceColour == Black ? QPixmap("swartPerd.png").scaled(40, 40) :
+                                          QPixmap("witPerd.png").scaled(40, 40));
 
 }
 
 QList<Position> HorsePiece::possibleMoves()
 {
+    Position p;
     QList<Position> moves;
+
+    PieceColour oppositeColour;
+
+    if (pieceColour == Black)
+    {
+        oppositeColour = White;
+    }
+    else if (pieceColour == White)
+    {
+        oppositeColour = Black;
+    }
+
+    // 2 regs, 1 op
+    if (board[x+2][y-1] == nullptr)
+    {
+        p.x = x+2;
+        p.y = y-1;
+        moves.append(p);
+    }
+    if (board[x+2][y-1] && board[x+2][y-1]->colour() == oppositeColour)
+    {
+        p.x = x+2;
+        p.y = y-1;
+        moves.append(p);
+    }
+
+    // 2 regs, 1 af
+    if (board[x+2][y+1] == nullptr)
+    {
+        p.x = x+2;
+        p.y = y+1;
+        moves.append(p);
+    }
+    if (board[x+2][y+1] && board[x+2][y+1]->colour() == oppositeColour)
+    {
+        p.x = x+2;
+        p.y = y+1;
+        moves.append(p);
+    }
+
+    // 2 af, 1 regs
+    if (board[x+1][y+2] == nullptr)
+    {
+        p.x = x+1;
+        p.y = y+2;
+        moves.append(p);
+    }
+    if (board[x+1][y+2] && board[x+1][y+2]->colour() == oppositeColour)
+    {
+        p.x = x+1;
+        p.y = y+2;
+        moves.append(p);
+    }
+
+    // 2 af, 1 links
+    if (board[x-1][y+2] == nullptr)
+    {
+        p.x = x-1;
+        p.y = y+2;
+        moves.append(p);
+    }
+    if (board[x-1][y+2] && board[x-1][y+2]->colour() == oppositeColour)
+    {
+        p.x = x-1;
+        p.y = y+2;
+        moves.append(p);
+    }
+
+    // 2 links, 1 af
+    if (board[x-2][y+1] == nullptr)
+    {
+        p.x = x-2;
+        p.y = y+1;
+        moves.append(p);
+    }
+    if (board[x-2][y+1] && board[x-2][y+1]->colour() == oppositeColour)
+    {
+        p.x = x-2;
+        p.y = y+1;
+        moves.append(p);
+    }
+
+    // 2 links, 1 op
+    if (board[x-2][y-1] == nullptr)
+    {
+        p.x = x-2;
+        p.y = y-1;
+        moves.append(p);
+    }
+    if (board[x-2][y-1] && board[x-2][y-1]->colour() == oppositeColour)
+    {
+        p.x = x-2;
+        p.y = y-1;
+        moves.append(p);
+    }
+
+    // 2 op, 1 links
+    if (board[x-1][y-2] == nullptr)
+    {
+        p.x = x-1;
+        p.y = y-2;
+        moves.append(p);
+    }
+    if (board[x-1][y-2] && board[x-1][y-2]->colour() == oppositeColour)
+    {
+        p.x = x-1;
+        p.y = y-2;
+        moves.append(p);
+    }
+
+    // 2 op, 1 regs
+    if (board[x+1][y-2] == nullptr)
+    {
+        p.x = x+1;
+        p.y = y-2;
+        moves.append(p);
+    }
+    if (board[x+1][y-2] && board[x+1][y-2]->colour() == oppositeColour)
+    {
+        p.x = x+1;
+        p.y = y-2;
+        moves.append(p);
+    }
+
+
     return moves;
 }
